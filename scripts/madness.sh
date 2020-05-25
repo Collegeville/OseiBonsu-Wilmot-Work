c0119eda7bc9b4449fe6af189550c3988eacaa00
wbonsu <34726639+wbonsu@users.noreply.github.com>
2020-05-25 13:09:37 -0400

    Update madness.sh

--- scripts/madness.sh
+++ scripts/madness.sh
@@ -6,7 +6,7 @@ mkdir madness
 cd ./madness
 yes| apt-get install cmake 
 git init
-git pull  http://github.com/wbonsu/MarchMaddness.git
+git pull http://github.com/wbonsu/MarchMaddness.git
 cmake . 
 make
 ./March_Madness_Simulator 0 > output_no_advantage.txt
